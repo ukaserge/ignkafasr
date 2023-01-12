8679374fc04f04b628a8556498659bf8600144b2
limdongjin <geniuslim27@gmail.com>
2023-01-13 04:21:38 +0900

    remove unused file

--- ignasr-app/image_build_script.sh
+++
@@ -1,9 +0,0 @@
-# 0.
-./mvnw clean 
-
-# 1. 
-./mvnw -DskipTests package
-
-# 2. 
-# change version of image
-./mvnw -DskipTests com.google.cloud.tools:jib-maven-plugin:build -Dimage=gcr.io/limdongjin-kube/ignasr:v3
